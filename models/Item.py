d1a7a93c23e1a2afef20ba6e2941a4a6391ef3d0
Kevin Morris <kevr.gtalk@gmail.com>
2020-02-02 18:37:04 -0800

    Skeleton of api POST request json
    This will need to be finished as we gain functionality in the item
    class to do so, however, pretty much every type of item in the game
    can be searched by this method. There are comments throughout the
    get_json() function which explain cases for different types
    of items we can request.
    
    To finish this is really just ifing out the type of item and changing
    what we fill depending on what we have. Most of the differences
    here are involved with the name/type fields.
    
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

--- models/Item.py
+++ models/Item.py
@@ -39,4 +39,196 @@ class Item:
         raise NotImplementedError
 
     def get_json(self):
+        # For items
+        data = {
+            "query": {
+
+                # Required, we only wanna look at things that are online
+                # and recently posted.
+                "status": {
+                    "option": "online"
+                },
+
+                # For all items, a type (base) is required. For unique items,
+                # we must also include the name of the item.
+                "name": "Kaom's Sign",
+                # The actual type of the item -- an item base.
+                "type": "Coral Ring",
+
+                # When we're searching a map, there is no name, only a type
+                # that has an option field, accompanied by a discriminate.
+                # The discriminate in the current league's case is "warfortheatlas"
+                "type": {
+                    "option": "Ghetto Map",
+                    "discriminate": "warfortheatlas"
+                },
+
+                # When we're searching a prophecy, "Prophecy" is required
+                # as the type, and it's name is required.
+                "name": "Fated Connections",
+                "type": "Prophecy",
+
+                # A flask is treated just the same as items are. See line 50.
+
+                # Gems can be found by just searching it's type. For gems,
+                # quality and gem level are quite important -- see
+                # misc_filters below.
+                "type": "Fireball",
+
+                # Jewels are treated just the same as items are. See line 50.
+
+                # A metamorph sample. The request with one of these types
+                # should probably be accompanied by stats for direct matching.
+                "type": "Metamorph Brain",
+                "type": "Metamorph Liver",
+                "type": "Metamorph Lung",
+                "type": "Metamorph Heart",
+                "type": "Metamorph Eye",
+
+                # Captured Beasts are a totally different story... they have
+                # different fields than I've seen on any other items, with
+                # Genus, Group, and Family. Not sure what we should do here.
+
+                # Stats to search for; When we're including no stats in our
+                # search, the minimum standard value given for stats would be:
+                # [{ "type": "and", "filters": [] }]
+                "stats": [
+                    {
+                        "type": "and",
+                        "filters": [   
+                            {
+                                "id": "explicit.stat_3509831",
+                                "value": {
+                                    "min": 1,
+                                    "max": 999
+                                }
+                            },
+                            {
+                                "id": "pseudo.pseudo_total_life",
+                                "value": {
+                                    "min": 23,
+                                    "max": 999
+                                }
+                            },
+                            # ... any more mods here.
+                        ]
+                    },
+
+                    # This next one could be another section of stats
+                    # to be searching for; when normally trading,
+                    # we could use this to specify a set of stats for "count"
+                    # etc, but when we're predicting things, we won't do this.
+                    {
+                        "type": "count",
+                        "value": {
+                            "min": 1,
+                            "max": 1
+                        },
+                        "filters": []
+                    }
+                ],
+
+                # Filters specified here are all options on the left-side
+                # of pathofexile.com/trade's UI.
+                "filters": {
+                    "socket_filters": {
+                        "filters": {
+                            "links": {
+                                "min": 6,
+                                "max": 6
+                            }
+                        }
+                    },
+
+                    # For certain item attributes, we must specify misc_filters
+                    # This field is quite important for gems or bases.
+                    "misc_filters": {
+                        "quality": {
+                            "min": 20,
+                            "max": 20
+                        },
+                        "gem_level": {
+                            "min": 21,
+                            "max": 21
+                        },
+                        # Gem experience
+                        "gem_level_progress": {
+                            "min": 0,
+                            "max": 666
+                        },
+
+                        # Influences should be given in this section.
+                        "shaper_item": {
+                            "option": "true"
+                        },
+
+                        # If "Any" is specified, these specifications with
+                        # { "option": "true" } should be omitted.
+                        "corrupted": {
+                            "option": "true"
+                        },
+
+                        # Veiled items
+                        "veiled": {
+                            "option": "true"
+                        }
+                    }
+                }
+            },
+            # We always want to sort in ascending order; this should be
+            # included with every search payload.
+            "sort": {
+                "price": "asc"
+            }
+        }
+
+        # For exchange, which includes: currency, fragments, catalysts, oils,
+        # incubators, scarabs, delve resonators, delve fossils, vials,
+        # essences, cards and maps.
+        data = {
+            "exchange": {
+                "status": {
+                    "option": "online"
+                },
+                # Whatever currency we want to count
+                "have": ["chaos"],
+
+                # Map format
+                "want": ["beach-map-tier-1"],
+
+                # Divination Cards
+                "want": ["a-dab-of-ink"],
+
+                # Essences
+                "want": ["muttering-essence-of-hatred"],
+
+                # Vials
+                "want": ["vial-of-dominance"],
+
+                # Fossils
+                "want": ["scorched-fossil"],
+
+                # Resonators
+                "want": ["primitive-alchemical-resonator"],
+
+                # Scarabs
+                "want": ["rusted-breach-scarab"],
+
+                # Incubators
+                "want": ["fine-incubator"],
+
+                # Oils
+                "want": ["silver-oil"],
+
+                # Catalysts
+                "want": ["turbulent-catalyst"],
+
+                # We should look up fragments as items in the Item
+                # alternative request above. Seems that fragments
+                # are named quite oddly in the exchange trade site,
+                # and searching them up as items should be fine.
+                # In the fragment case, we should just supply the fragment
+                # name as the "type" field of a query.
+            },
+        }
         raise NotImplementedError
